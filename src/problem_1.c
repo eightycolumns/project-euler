aa12f6a1ef8684531a641ae4359591043f82a383
Drew Keller <drew@eightycolumns.com>
2016-08-21 09:04:01 -0400

    Extract sum_of_multiples_of()

--- src/problem_1.c
+++ src/problem_1.c
@@ -1,29 +1,24 @@
 #include <inttypes.h>
 #include <stdio.h>
 
+int32_t sum_of_multiples_of(int8_t common_difference);
+
 int main(void) {
   int32_t answer = 0;
 
-  int8_t common_difference = 3;
+  answer += sum_of_multiples_of(3);
+
+  answer += sum_of_multiples_of(5);
+
+  answer -= sum_of_multiples_of(15);
+
+  printf("1) %" PRId32 "\n", answer);
+}
+
+int32_t sum_of_multiples_of(int8_t common_difference) {
   int16_t n = 999 / common_difference;
   int8_t first_term = common_difference;
   int16_t nth_term = first_term + (n - 1) * common_difference;
   int32_t nth_partial_sum = n * (first_term + nth_term) / 2;
-  answer += nth_partial_sum;
-
-  common_difference = 5;
-  n = 999 / common_difference;
-  first_term = common_difference;
-  nth_term = first_term + (n - 1) * common_difference;
-  nth_partial_sum = n * (first_term + nth_term) / 2;
-  answer += nth_partial_sum;
-
-  common_difference = 15;
-  n = 999 / common_difference;
-  first_term = common_difference;
-  nth_term = first_term + (n - 1) * common_difference;
-  nth_partial_sum = n * (first_term + nth_term) / 2;
-  answer -= nth_partial_sum;
-
-  printf("1) %" PRId32 "\n", answer);
+  return nth_partial_sum;
 }
