90bad06da2f622e82c1137386cd239c7932e1442
Drew Keller <drew@eightycolumns.com>
2016-08-21 08:48:25 -0400

    Introduce first_term

--- src/problem_1.c
+++ src/problem_1.c
@@ -5,13 +5,16 @@ int main(void) {
   int32_t answer = 0;
 
   int16_t n = 999 / 3;
-  answer += n * (3 + 3 + (n - 1) * 3) / 2;
+  int8_t first_term = 3;
+  answer += n * (first_term + first_term + (n - 1) * 3) / 2;
 
   n = 999 / 5;
-  answer += n * (5 + 5 + (n - 1) * 5) / 2;
+  first_term = 5;
+  answer += n * (first_term + first_term + (n - 1) * 5) / 2;
 
   n = 999 / 15;
-  answer -= n * (15 + 15 + (n - 1) * 15) / 2;
+  first_term = 15;
+  answer -= n * (first_term + first_term + (n - 1) * 15) / 2;
 
   printf("1) %" PRId32 "\n", answer);
 }
