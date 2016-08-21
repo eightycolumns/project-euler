e4344cc3237aeacd31e376926d40927e1a59aa6b
Drew Keller <drew@eightycolumns.com>
2016-08-21 09:10:06 -0400

    Extract nth_partial_sum()

--- src/problem_1.c
+++ src/problem_1.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int32_t sum_of_multiples_of(int8_t common_difference);
+int32_t nth_partial_sum(int16_t n, int8_t first_term, int16_t nth_term);
 
 int main(void) {
   int32_t answer = 0;
@@ -19,5 +20,9 @@ int32_t sum_of_multiples_of(int8_t common_difference) {
   int8_t first_term = common_difference;
   int16_t nth_term = first_term + (n - 1) * common_difference;
 
+  return nth_partial_sum(n, first_term, nth_term);
+}
+
+int32_t nth_partial_sum(int16_t n, int8_t first_term, int16_t nth_term) {
   return n * (first_term + nth_term) / 2;
 }
