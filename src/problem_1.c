2ea731cfc2535e0f70bab6802ea6238a3e84b33b
Drew Keller <drew@eightycolumns.com>
2016-08-21 00:18:36 -0400

    Extract is_divisible_by()

--- src/problem_1.c
+++ src/problem_1.c
@@ -1,14 +1,21 @@
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdio.h>
+
+bool is_divisible_by(int16_t a, int8_t b);
 
 int main(void) {
   int32_t answer = 0;
 
   for (int16_t i = 0; i < 1000; i += 1) {
-    if (i % 3 == 0 || i % 5 == 0) {
+    if (is_divisible_by(i, 3) || is_divisible_by(i, 5)) {
       answer += i;
     }
   }
 
   printf("1) %" PRId32 "\n", answer);
+}
+
+bool is_divisible_by(int16_t a, int8_t b) {
+  return a % b == 0;
 }
