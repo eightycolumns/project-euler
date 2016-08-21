3355510eb9c5ecf94c52a9068f68055c8b3f9f02
Drew Keller <drew@eightycolumns.com>
2016-08-21 09:13:19 -0400

    Replace 15 with lcm(3, 5)

--- src/problem_1.c
+++ src/problem_1.c
@@ -1,8 +1,10 @@
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 int32_t sum_of_multiples_of(int8_t common_difference);
 int32_t nth_partial_sum(int16_t n, int8_t first_term, int16_t nth_term);
+int8_t lcm(int8_t a, int8_t b);
 
 int main(void) {
   int32_t answer = 0;
@@ -10,7 +12,7 @@ int main(void) {
   answer += sum_of_multiples_of(3);
   answer += sum_of_multiples_of(5);
 
-  answer -= sum_of_multiples_of(15);
+  answer -= sum_of_multiples_of(lcm(3, 5));
 
   printf("1) %" PRId32 "\n", answer);
 }
@@ -25,4 +27,20 @@ int32_t sum_of_multiples_of(int8_t common_difference) {
 
 int32_t nth_partial_sum(int16_t n, int8_t first_term, int16_t nth_term) {
   return n * (first_term + nth_term) / 2;
+}
+
+int8_t lcm(int8_t a, int8_t b) {
+  int8_t step = (a > b) ? a : b;
+
+  int8_t lcm = step;
+
+  while (true) {
+    if (lcm % a == 0 && lcm % b == 0) {
+      break;
+    }
+
+    lcm += step;
+  }
+
+  return lcm;
 }
