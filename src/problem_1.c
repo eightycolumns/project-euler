3f5a869448c5dec7d49575b9b606f077dd29c36c
Drew Keller <drew@eightycolumns.com>
2016-08-21 09:05:39 -0400

    Adjust vertical whitespace

--- src/problem_1.c
+++ src/problem_1.c
@@ -7,7 +7,6 @@ int main(void) {
   int32_t answer = 0;
 
   answer += sum_of_multiples_of(3);
-
   answer += sum_of_multiples_of(5);
 
   answer -= sum_of_multiples_of(15);
@@ -20,5 +19,6 @@ int32_t sum_of_multiples_of(int8_t common_difference) {
   int8_t first_term = common_difference;
   int16_t nth_term = first_term + (n - 1) * common_difference;
   int32_t nth_partial_sum = n * (first_term + nth_term) / 2;
+
   return nth_partial_sum;
 }
