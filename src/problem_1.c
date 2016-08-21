9b0d19ff66dece7cf2ff7494ab74cbb7305efb5d
Drew Keller <drew@eightycolumns.com>
2016-08-21 08:57:45 -0400

    Introduce nth_partial_sum

--- src/problem_1.c
+++ src/problem_1.c
@@ -8,19 +8,22 @@ int main(void) {
   int16_t n = 999 / common_difference;
   int8_t first_term = common_difference;
   int16_t nth_term = first_term + (n - 1) * common_difference;
-  answer += n * (first_term + nth_term) / 2;
+  int32_t nth_partial_sum = n * (first_term + nth_term) / 2;
+  answer += nth_partial_sum;
 
   common_difference = 5;
   n = 999 / common_difference;
   first_term = common_difference;
   nth_term = first_term + (n - 1) * common_difference;
-  answer += n * (first_term + nth_term) / 2;
+  nth_partial_sum = n * (first_term + nth_term) / 2;
+  answer += nth_partial_sum;
 
   common_difference = 15;
   n = 999 / common_difference;
   first_term = common_difference;
   nth_term = first_term + (n - 1) * common_difference;
-  answer -= n * (first_term + nth_term) / 2;
+  nth_partial_sum = n * (first_term + nth_term) / 2;
+  answer -= nth_partial_sum;
 
   printf("1) %" PRId32 "\n", answer);
 }
