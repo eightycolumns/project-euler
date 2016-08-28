9057c1df3d24f67da3e1d4cbea2825e2b8814c80
Drew Keller <drew@eightycolumns.com>
2016-08-28 17:33:24 -0400

    Extract is_divisible_by()

--- src/problem_3.c
+++ src/problem_3.c
@@ -1,12 +1,15 @@
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdio.h>
+
+bool is_divisible_by(int64_t a, int16_t b);
 
 int main(void) {
   int64_t number = 600851475143;
   int16_t factor = 2;
 
   while (number > 1) {
-    while (number % factor == 0) {
+    while (is_divisible_by(number, factor)) {
       number /= factor;
     }
 
@@ -16,4 +19,8 @@ int main(void) {
   int16_t answer = factor - 1;
 
   printf("3) %" PRId16 "\n", answer);
+}
+
+bool is_divisible_by(int64_t a, int16_t b) {
+  return a % b == 0;
 }
