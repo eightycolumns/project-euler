bad48340e6dac7bca47f971f7388a6d3b428d734
Drew Keller <drew@eightycolumns.com>
2016-08-28 17:28:21 -0400

    Improve solution to problem 2

--- src/problem_2.c
+++ src/problem_2.c
@@ -1,28 +1,19 @@
 #include <inttypes.h>
-#include <stdbool.h>
 #include <stdio.h>
-
-bool is_even(int32_t d);
 
 int main(void) {
   int32_t answer = 0;
 
   int32_t previous = 0;
-  int32_t current = 1;
+  int32_t current = 2;
 
   while (current <= 4000000) {
-    if (is_even(current)) {
-      answer += current;
-    }
+    answer += current;
 
-    int32_t next = current + previous;
+    int32_t next = 4 * current + previous;
     previous = current;
     current = next;
   }
 
   printf("2) %" PRId32 "\n", answer);
-}
-
-bool is_even(int32_t d) {
-  return d % 2 == 0;
 }
